e33ac56148e63fdb0b8b53c0f233e9b2edfc1486
Andrea Vecchione <andrea.vecchione02@gmail.com>
2024-09-23 01:48:45 +0200

    EsercizioSpring6

---
+++ src/main/java/com/example/Esercizio6/entities/Class.java
@@ -0,0 +1,21 @@
+package com.example.Esercizio6.entities;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table (name = "classes")
+
+public class Class {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private long id;
+    @Column (nullable = false)
+    private String title;
+    @Column (nullable = false)
+    private String description;
+    @ManyToMany
+    private List<Student> students;
+}
---
+++ src/main/java/com/example/Esercizio6/entities/Student.java
@@ -0,0 +1,25 @@
+package com.example.Esercizio6.entities;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table (name = "students")
+
+public class Student {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private long id;
+    @Column (nullable = false)
+    private String lastName;
+    @Column (nullable = false)
+    private String firstName;
+    @Column (nullable = false, unique = true)
+    private String email;
+
+    @ManyToMany
+    @JoinTable (name = "enrollments",
+    joinColumns = @JoinColumn (name = "Student_id"),
+    inverseJoinColumns = @JoinColumn (name = "class_id"))
+    private List<Class> corsi;
+}
